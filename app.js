18af82dc1809c44931bf457966907372baefb7e7
= <sameer.pathan@peopleinteractive.in>
2019-10-28 21:01:57 +0530

    returning dummy user profile on /profile

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const dummyProfile = {
+	'name': 'Mark Ross',
+	'age': 25, 
+	'occupation': 'Programmer',
+	'country': 'India',
+	'organization': 'Microsoft'
+}
+
+app.get('/profile', (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
+	res.json(dummyProfile);
+})
+app.listen(8050, () => {
+	console.log('app listening on port 8050');
+})
